ce0acb89e155298100b4b1dc5cd6addfd136a53d
William Furr <william.furr@gmail.com>
2017-01-29 09:34:53 -0500

    Add TAX_RATE constant, formatting

--- src/server.js
+++ src/server.js
@@ -2,19 +2,24 @@
  *  Blue loot is handled by fixed NPC buy orders, not Jita price.
  */
 var BLUE_LOOT = {
-'Ancient Coordinates Database': 1500000,
-'Neural Network Analyzer': 200000,
-'Sleeper Data Library': 500000,
-'Sleeper Drone AI Nexus': 5000000
+  'Ancient Coordinates Database': 1500000,
+  'Neural Network Analyzer': 200000,
+  'Sleeper Data Library': 500000,
+  'Sleeper Drone AI Nexus': 5000000
 };
+
+/**
+ *  Tax rate to apply to loot.  Payout will be 1 - TAX_RATE.
+ */
+var TAX_RATE = 0.15;
 
 /**
  *  Main rendering function for GET request
  */
 function doGet() {
-	return HtmlService.createHtmlOutputFromFile('calc.html')
-		.addMetaTag('viewport', 'width=device-width, initial-scale=1')
-		.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return HtmlService.createHtmlOutputFromFile('calc.html')
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
 /**
@@ -23,26 +28,26 @@ function doGet() {
  *  Returns false for fetch error or no Evepraisal ID in response.
  */
 function makeEvepraisal(loot) {
-	var resp;
+  var resp;
   try {
-		resp = UrlFetchApp.fetch('http://evepraisal.com/estimate', {
-				method: 'post',
-				payload: {
-					raw_paste: loot,
-					hide_buttons: false,
-					paste_autosubmit: false,
-					market: '30000142',  // Jita
-					save: true
-				}
-			}).getContentText();
+    resp = UrlFetchApp.fetch('http://evepraisal.com/estimate', {
+      method: 'post',
+      payload: {
+        raw_paste: loot,
+        hide_buttons: false,
+        paste_autosubmit: false,
+        market: '30000142',  // Jita
+        save: true
+      }
+    }).getContentText();
   } catch(error) {
     return false;
   }
-	var matches = resp.match(/document.title = "Evepraisal - Result #([0-9]+)/);
-	if (!matches) {
-		return false;
-	}
-	return 'http://evepraisal.com/e/' + matches[1];
+  var matches = resp.match(/document.title = "Evepraisal - Result #([0-9]+)/);
+  if (!matches) {
+    return false;
+  }
+  return 'http://evepraisal.com/e/' + matches[1];
 }
 
 /**
@@ -51,33 +56,33 @@ function makeEvepraisal(loot) {
  *  Handles both evepraisal URL or inventory block.
  */
 function calculatePayout(loot) {
-	var evepraisal_url = loot.match('http://evepraisal.com/e/[0-9]+');
-	if (!evepraisal_url) {
-		evepraisal_url = makeEvepraisal(loot);
-		if (!evepraisal_url) {
-			return { error: "Couldn't make Evepraisal for items" };
-		}
-	}
-	var json;
-	try {
-		json = UrlFetchApp.fetch(evepraisal_url + '.json');
-	} catch(error) {
-		return { error: "Couldn't fetch Evepraisal JSON for URL: " + evepraisal_url };
-	}
-	var evepraisal = JSON.parse(json);
+  var evepraisal_url = loot.match('http://evepraisal.com/e/[0-9]+');
+  if (!evepraisal_url) {
+    evepraisal_url = makeEvepraisal(loot);
+    if (!evepraisal_url) {
+      return { error: "Couldn't make Evepraisal for items" };
+    }
+  }
+  var json;
+  try {
+    json = UrlFetchApp.fetch(evepraisal_url + '.json');
+  } catch(error) {
+    return { error: "Couldn't fetch Evepraisal JSON for URL: " + evepraisal_url };
+  }
+  var evepraisal = JSON.parse(json);
 
-	var payout = 0;
-	for (i = 0, leni = evepraisal.items.length; i < leni; i++) {
-		var item = evepraisal.items[i];
-		if (item.name in BLUE_LOOT) {
-			payout += BLUE_LOOT[item.name] * item.quantity * 0.85;
-		} else {
-			payout += item.prices.buy.max * item.quantity * 0.85;
-		}
-	}
+  var payout = 0;
+  for (i = 0, leni = evepraisal.items.length; i < leni; i++) {
+    var item = evepraisal.items[i];
+    if (item.name in BLUE_LOOT) {
+      payout += BLUE_LOOT[item.name] * item.quantity * (1 - TAX_RATE);
+    } else {
+      payout += item.prices.buy.max * item.quantity * (1 - TAX_RATE);
+    }
+  }
 
-	return {
-		payout: payout,
-		evepraisal_url: evepraisal_url
-	};
+  return {
+    payout: payout,
+    evepraisal_url: evepraisal_url
+  };
 }
